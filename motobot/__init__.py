4739bff2ca0f0fa145607f7bf6182304bcf137e8
Motoko11 <robertswaine7@gmail.com>
2016-02-29 10:16:46 +0000

    remove core files from desubot repo

--- motobot/__init__.py
+++
@@ -1,9 +0,0 @@
-from .irc_level import IRCLevel
-from .irc_message import IRCMessage
-from .irc_bot import IRCBot
-from .decorators import hook, command, match, sink, request
-from .priority import Priority
-from .modifiers import Modifier, Action, CTCP, Target, Command, Notice, EatModifier, Eat
-from .utilities import split_response, Context
-
-__VERSION__ = '0.2'
