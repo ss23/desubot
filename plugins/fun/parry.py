63fec2b2cb5c2dd49fcf25f713c7eea1f6bc28c1
Motoko11 <robertswaine7@gmail.com>
2016-03-04 07:11:40 +0000

    Adds parry response

---
+++ plugins/fun/parry.py
@@ -0,0 +1,14 @@
+from motobot import match, Action
+from random import uniform
+
+
+@match(r'\*(.+? )(stabs) desubot')
+def stab_match(bot, context, message, match):
+    if uniform(0, 100) >= 50:
+        if uniform(0, 100) >= 50:
+            response = "parries {}".format(context.nick)
+        else:
+            response = "ripostes {}".format(context.nick)
+    else:
+        response = "dies"
+    return response, Action
