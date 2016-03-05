87b1e81fd8a0fcb1cbe1dfaa7e9bc83f482cf657
Motoko11 <robertswaine7@gmail.com>
2016-03-05 04:58:56 +0000

    Adds calc module

---
+++ plugins/utility/calc.py
@@ -0,0 +1,26 @@
+from motobot import command
+from requests import get
+from xmltodict import parse
+
+
+@command('calc')
+def calc_command(bot, context, message, args):
+    query = ' '.join(args[1:])
+    result = get_result(query, bot.wolfram_api_key)
+    return result
+
+
+def get_result(query, api_key):
+    try:
+        url = 'http://api.wolframalpha.com/v2/query'
+        params = {
+            'appid': api_key,
+            'includepodid': 'Result',
+            'input': query
+        }
+        response = parse(get(url, params=params).text)
+        result = response['queryresult']['pod']['subpod']['plaintext']
+        result = "Calculation Result: {}".format(result)
+    except:
+        result = "Error calculating result."
+    return result
