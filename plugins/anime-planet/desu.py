d7e1f72fcab16d05a5ad568335569fffec911c47
Motoko11 <robertswaine7@gmail.com>
2016-12-13 02:45:34 +0000

    Change desu frequencies

--- plugins/anime-planet/desu.py
+++ plugins/anime-planet/desu.py
@@ -12,11 +12,11 @@ def desu_match(bot, context, message, match):
         chance = uniform(0, 1)
         number = randint(0, 30)
         string = ''
-        un = chance <= 0.01
+        un = chance <= 0.025
 
         if un:
-            if chance <= 0.01:
-                special_desus = ['omgdesu', 'dechu']
+            if uniform(0, 1) <= 0.2:
+                special_desus = ['omgdesu', 'dechu', 'deshu', 'http://media.japanpowered.com/images/desu-meme.jpg']
                 string = choice(special_desus)
             else:
                 string = 'undesu'
